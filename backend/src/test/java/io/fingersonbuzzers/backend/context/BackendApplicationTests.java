f9532be59efc05d76249beb3604c8d64c302a50e
James Garden <jamesegarden@outlook.com>
2023-04-10 15:18:46 +0100

    Updated AbstractIntegrationTest

--- backend/src/test/java/io/fingersonbuzzers/backend/context/BackendApplicationTests.java
+++ backend/src/test/java/io/fingersonbuzzers/backend/context/BackendApplicationTests.java
@@ -1,15 +1,10 @@
 package io.fingersonbuzzers.backend.context;
 
 import io.fingersonbuzzers.backend.AbstractIntegrationTest;
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BackendApplicationTests extends AbstractIntegrationTest {
-
-  @Autowired
-  EntityManager entityManager;
 
   @Test
   void contextLoads() {
