46643d67801b34cf16b81cb8f06cff18f71a483c
James Garden <jamesegarden@outlook.com>
2023-09-16 13:52:12 +0100

    Initial commit

---
+++ API/src/test/java/io/fingersonbuzzers/api/ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.fingersonbuzzers.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertThat(1+1).isEqualTo(2);
+  }
+
+}
